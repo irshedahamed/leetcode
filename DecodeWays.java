b059620f38bc536455c8947e9fb727d68d306e7f
Mohamed Irshed Ahamed <mohammedirshed15@gmail.com>
2020-12-30 01:50:56 +0530

    Update DecodeWays.java

--- DecodeWays.java
+++ DecodeWays.java
@@ -16,9 +16,11 @@ Explanation: It could be decoded as "AB" (1 2) or "L" (12).
 
 class Solution {
     public int numDecodings(String s) {
+        if(s.charAt(0)=='0')
+            return 0;
         int[] dp = new int[s.length()+1];
         dp[0] = 1;
-        dp[1] = (s.charAt(0)=='0') ? 0 : 1;
+        dp[1] = 1;
         
         for(int i = 2 ;i<=s.length();i++){
             if(s.charAt(i-1)>'0'){
