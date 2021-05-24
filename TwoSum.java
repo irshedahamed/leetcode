5d2d719dad1d582ebf3d6c68d9e2bf9bcc66f8eb
irshedahamed <mohammedirshed15@gmail.com>
2021-05-24 23:34:56 +0530

    Updated Two Sum

--- TwoSum.java
+++ TwoSum.java
@@ -17,13 +17,10 @@ class Solution {
         int result = 0;
         Map<Integer,Integer> map = new HashMap<>();
         for(int i=0;i<nums.length;i++){
-            map.put(nums[i],i);
-        }
-        
-        for(int i=0;i<nums.length;i++){
             if(map.containsKey(target-nums[i]) && map.get(target-nums[i])!=i){
                 return new int[] {i,map.get(target-nums[i])};
             }
+            map.put(nums[i],i);
         }
         return new int[] {0,0};
     }
