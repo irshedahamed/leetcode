3624b87d9c50cd950915549d7be84bb8314236d0
Mohamed Irshed Ahamed <mohammedirshed15@gmail.com>
2022-07-13 19:45:59 +0530

    Create ContainsDuplicate.java

---
+++ Prob-150/ContainsDuplicate.java
@@ -0,0 +1,11 @@
+class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int num : nums){
+            if(set.contains(num))
+                return true;
+            set.add(num);
+        }
+        return false;
+    }
+}
